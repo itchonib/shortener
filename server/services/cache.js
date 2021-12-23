b90d10a794292bd614c1a04550890967a9c89154
Ianne Itchon <itchoib@gmail.com>
2021-12-22 23:12:43 -0500

    cleaned up cache file

--- server/services/cache.js
+++ server/services/cache.js
@@ -1,79 +1,61 @@
-const mongoose = require('mongoose');
-const redis = require('redis');
-const util = require('util');
-// const path = require("path")
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
 
-// require('dotenv').config({path: path.resolve(__dirname+'../../.env')})
-
-const client = redis.createClient({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT});
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
 client.hget = util.promisify(client.hget);
 
-mongoose.Query.prototype.cache = function(hkey){
-    this.useCache = true;
+client.on("connect", () => {
+  console.log("Redis is connected");
+});
 
-    // this is the top level key like motercycles or cars or trucks etc
-    this.hashkey = JSON.stringify(hkey || '')
+client.on("end", () => {
+  console.log("Redis has disconnected");
+});
 
-    return this;
-}
+mongoose.Query.prototype.cache = function (hkey) {
+  this.useCache = true;
+  this.hashkey = JSON.stringify(hkey || "");
+  return this;
+};
 
-// We are storing the default exec() function in the exec variable
-const exec = mongoose.Query.prototype.exec 
+const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = async function(){ // Modifing the exec property of mongoose
-    // this = mongoose.Query.prototype.exec
-    // When useCache = false we should directly send the query to MongoDB and return the result to app.js
-    if(!this.useCache){
-        return exec.apply(this, arguments)
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  let key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  const cacheValue = await client.hget(this.hashkey, key);
+
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+    console.log("found in cache", doc);
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments);
+
+  if (result) {
+    if (Array.isArray(result) && result.length == 0) {
+      return null;
+    } else {
+      client.hset(this.hashkey, key, JSON.stringify(result));
+      return result;
     }
-
-    /* Here is how our key looks
-     * key = '{query_param_1: param_1_value, query_param_2: param_2_value,...... , collectoin: collection name}'
-     * we need to stringigy the object before storing in redis cache
-    */
-    let key = JSON.stringify(Object.assign({},this.getQuery(),{collection: this.mongooseCollection.name}));
-
-    /* Querying the cache
-     * if value for key exists then, cacheValue = data
-     * else, cacheValue = null
-    */
-    const cacheValue = await client.hget(this.hashkey, key)
-    console.log("*******key******", this.hashkey, key)
-    console.log("***************", cacheValue)
-    
-    // When data is found in redis cache
-    if(cacheValue){
-        const doc = JSON.parse(cacheValue)  // converting back to original datatype from string
-
-        /* While storing data in redis we may store a single object or an array of objects. 
-         * We need to convert normal json into mongoose model instance before returning to app.js, 
-         * this.model() is used for this purpose
-        */
-        return  Array.isArray(doc)
-                ? doc.map((d)=>new this.model(d))
-                : new this.model(doc);
-    }
-
-    // Data not present in redis cache, get the data from Mongodb and save the data to redis cache also
-    const result = await exec.apply(this, arguments) // using the default exec function
-
-    // just some logic to check if the data for the required query is even present in the database
-    if(result){ // mongodb retured non-null value (can be empty array)
-        if(Array.isArray(result) && result.length==0){
-            // array is empty
-            return null
-        }
-        else{
-            // data is there (non-empty array or an single object)
-            client.hset(this.hashkey, key, JSON.stringify(result)); // saving data in redis cache
-            return result
-        }
-    }else{ // database returned null value
-        console.log("data not present")
-        return null
-    } 
-}
-
-module.exports =  function clearCache(hashkey){
-    client.del(JSON.stringify(hashkey))
-}
+  } else {
+    return null;
+  }
+};
